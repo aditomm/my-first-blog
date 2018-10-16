c239b34eaa27fad209b338993b8cc1d3474368f8
Antonio Di Tommaso <antonio.ditommaso@dyrecta.com>
2018-10-16 09:06:45 +0200

    Ho cambiato l'HTML per questo sito.

--- mysite/settings.py
+++ mysite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe\Rome'
+TIME_ZONE = 'Europe/Rome'
 
 USE_I18N = True
 
@@ -101,5 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'db.sqlite3')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
